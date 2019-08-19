3772e9cabd260769a51be888b63c9575f2ce967e
Lex Serest <lexserest@gmail.com>
2019-08-19 23:09:48 +0300

    Upload

--- app.js
+++
@@ -1,7 +0,0 @@
-import './static/style.scss';
-import './game/main';
-
-
-try {
-  require('./tg-integration')
-} catch {}
--- game/helpers/enum.ts
+++
@@ -1,9 +0,0 @@
-export enum Direction { Up, Right, Down, Left }
-export enum Keys {
-  Up = 38,
-  Right = 37,
-  Left = 39,
-  Down = 40,
-  Space = 32,
-  Return = 13
-}
